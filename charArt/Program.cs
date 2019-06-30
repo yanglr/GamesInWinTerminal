189d05b649c2f0aad660e30261101066a1890cfe
Enjoy233 <legege007@126.com>
2019-06-30 17:52:51 +0800

    Add code.

---
+++ charArt/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace charArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("         :LOL:ROFL:ROFL");
+                Console.WriteLine("           A           ");
+                Console.WriteLine("       /--------       ");
+                Console.WriteLine(" LOL====      ()\\     ");
+                Console.WriteLine("  L    \\         \\        ");
+                Console.WriteLine("        \\_________]                ");
+                Console.WriteLine("           I     I      ");
+                Console.WriteLine("        -----------/              ");
+                Console.WriteLine("                       ");
+                Console.WriteLine("      John's ROFLCOPTER!              ");
+
+                System.Threading.Thread.Sleep(100);
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("ROFL:ROFL:LOL:         ");
+                Console.WriteLine("           A           ");
+                Console.WriteLine("  L    /--------       ");
+                Console.WriteLine(" LOL====      ()\\     ");
+                Console.WriteLine("       \\         \\        ");
+                Console.WriteLine("        \\_________]                ");
+                Console.WriteLine("           I     I      ");
+                Console.WriteLine("        -----------/              ");
+                Console.WriteLine("                       ");
+                Console.WriteLine("      John's ROFLCOPTER!              ");
+
+                System.Threading.Thread.Sleep(100);
+            }
+
+        }
+    }
+}
